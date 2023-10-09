b7cf270be0c8fdd0775f6b08ee772d271f6ec5ce
jesux8 <jesusalejowk@gmail.com>
2023-10-09 09:56:44 +0200

    vEnCuartoLugar

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace P2_2_Figuras
@@ -8,34 +8,35 @@ namespace P2_2_Figuras
     {
         static void Main(string[] args)
         {
-            // Crear instancias de las figuras
-            Color color = Color.FromArgb(255, 50, 20, 34);
-            Rectangulo rectangulo = new Rectangulo(color, 3.9, 3);
-            Circulo circulo = new Circulo(color, 5);
-            TrianguloEquilatero triangulo = new TrianguloEquilatero(color, 4);
+            // Crear una lista de figuras
+            List<Figura> lista_figuras = new List<Figura>();
+
+            // Crear instancias de figuras y agregarlas a la lista
+            Color colorRectangulo = Color.FromArgb(255, 50, 20, 34);
+            Rectangulo rectangulo = new Rectangulo(colorRectangulo, 3.9, 3);
+            lista_figuras.Add(rectangulo);
+
+            Color colorCirculo = Color.FromArgb(255, 230, 20, 30);
+            Circulo circulo = new Circulo(colorCirculo, 2);
+            lista_figuras.Add(circulo);
+
+            Color colorTriangulo = Color.FromArgb(255, 0, 20, 40);
+            TrianguloEquilatero triangulo = new TrianguloEquilatero(colorTriangulo, 3);
+            lista_figuras.Add(triangulo);
 
             // Mostrar las figuras y sus áreas
-            Console.WriteLine("Figuras Originales:");
-            MostrarFigura(rectangulo);
-            MostrarFigura(circulo);
-            MostrarFigura(triangulo);
-
-            // Modificar algunos parámetros y mostrar las figuras nuevamente
-            rectangulo.Base = 5;
-            circulo.Radio = 7;
-            triangulo.Lado = 6;
-
-            Console.WriteLine("\nFiguras Modificadas:");
-            MostrarFigura(rectangulo);
-            MostrarFigura(circulo);
-            MostrarFigura(triangulo);
+            Console.WriteLine("Lista de Figuras:");
+            foreach (Figura figura in lista_figuras)
+            {
+                MostrarFigura(figura);
+            }
         }
 
         // Método para mostrar la figura y su área
         static void MostrarFigura(Figura figura)
         {
             Console.WriteLine(figura.ToString());
-            Console.WriteLine($"Área: {figura.GetArea()}\n");
+            Console.WriteLine($"Área: {figura.GetArea():F2}\n");
         }
     }
 }
