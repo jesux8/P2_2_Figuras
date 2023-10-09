1476e337345cea654d6e6ed507f898dafe551873
jesux8 <jesusalejowk@gmail.com>
2023-10-09 09:43:31 +0200

    vEnTercerLugar

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Drawing;
 
 namespace P2_2_Figuras
@@ -7,20 +8,34 @@ namespace P2_2_Figuras
     {
         static void Main(string[] args)
         {
+            // Crear instancias de las figuras
             Color color = Color.FromArgb(255, 50, 20, 34);
-
             Rectangulo rectangulo = new Rectangulo(color, 3.9, 3);
             Circulo circulo = new Circulo(color, 5);
             TrianguloEquilatero triangulo = new TrianguloEquilatero(color, 4);
 
-            Console.WriteLine(rectangulo.ToString());
-            Console.WriteLine($"Área: {rectangulo.GetArea()}\n");
+            // Mostrar las figuras y sus áreas
+            Console.WriteLine("Figuras Originales:");
+            MostrarFigura(rectangulo);
+            MostrarFigura(circulo);
+            MostrarFigura(triangulo);
 
-            Console.WriteLine(circulo.ToString());
-            Console.WriteLine($"Área: {circulo.GetArea()}\n");
+            // Modificar algunos parámetros y mostrar las figuras nuevamente
+            rectangulo.Base = 5;
+            circulo.Radio = 7;
+            triangulo.Lado = 6;
 
-            Console.WriteLine(triangulo.ToString());
-            Console.WriteLine($"Área: {triangulo.GetArea()}");
+            Console.WriteLine("\nFiguras Modificadas:");
+            MostrarFigura(rectangulo);
+            MostrarFigura(circulo);
+            MostrarFigura(triangulo);
+        }
+
+        // Método para mostrar la figura y su área
+        static void MostrarFigura(Figura figura)
+        {
+            Console.WriteLine(figura.ToString());
+            Console.WriteLine($"Área: {figura.GetArea()}\n");
         }
     }
 }
